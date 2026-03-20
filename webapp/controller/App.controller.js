5a5eca986b5fbc317c7823bde3794d99cc17e748
vishnuteja0201 <134588072+vishnuteja0201@users.noreply.github.com>
2026-03-20 21:01:21 +0530

    step6:Modules

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,12 +1,12 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+], (Controller, MessageToast) => {
     "use strict";
 
     return Controller.extend("ui5.walkthrough.controller.App",{
         onShowHello(){
-            //show a native JavaScipt alert
-            alert("Hello world");
+            MessageToast.show("Hello World");
         },
 
         onBye(){
