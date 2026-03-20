2378c9fcbbc7e3d489ee120c952fdc5c127c01e7
vishnuteja0201 <134588072+vishnuteja0201@users.noreply.github.com>
2026-03-20 17:42:32 +0530

    step4: xml_views

--- webapp/index.js
+++ webapp/index.js
@@ -1,12 +1,11 @@
 sap.ui.define([
-"sap/m/Text"
-], (Text)=>{
+"sap/ui/core/mvc/XMLView"
+], (XMLView)=>{
     "use strict";
     
-
-    new Text({
-        text : "Hello SAP UI5"
-    }).placeAt("content");
+    XMLView.create({
+        viewName:"ui5.walkthrough.view.App"
+    }).then((oView)=> oView.placeAt("content"))
 
     console.log("working...........!!!");
     }
