10e441e252925cc49f9e6060590b4cb8f9c61109
vishnuteja0201 <vishnuteja0201@gmail.com>
2026-03-21 20:30:20 +0530

    step8_i18n

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,8 +1,9 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel"
-], (Controller, MessageToast, JSONModel) => {
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel"
+], (Controller, MessageToast, JSONModel, ResourceModel) => {
     "use strict";
 
     return Controller.extend("ui5.walkthrough.controller.App",{
@@ -12,22 +13,32 @@ sap.ui.define([
 
             const oModel = new JSONModel({
                 recipient: {
-                    name: "world"
+                    name: "Enter Your Name!"
                 }
             });
             this.getView().setModel(oModel);
+
+            const i18nModel = new ResourceModel({
+                bundleName: "ui5.walkthrough.i18n.i18n"
+            });
+            this.getView().setModel(i18nModel, "i18n");
 
             console.log("End of onInit() .........!!!");
         },
 
         onShowHello(){
-            MessageToast.show("Hello Vishnu");
+            //"Read msg from i18n model"
+            const oBundle = this.getView().getModel("i18n").getResourceBundle();
+            const sRecipient = this.getView().getModel().getProperty("/recipient/name");
+            const sMsg = oBundle.getText("helloMsg",[sRecipient]);
+            //show message
+            MessageToast.show(sMsg);
         },
 
         onBye(){
             //show a native JavaScipt alert
             alert("seee you soon");
-            console.log("Entered in controller.js.........!!!");
+            console.log("End of controller.js.........!!!");
 
         }
 
