ee5d9bb09752c8d144cb77c6bb41af91c47960ef
vishnuteja0201 <vishnuteja0201@gmail.com>
2026-03-25 14:14:38 +0530

    step10_descriptor(manifest.json)

--- webapp/Component.js
+++ webapp/Component.js
@@ -8,11 +8,7 @@ sap.ui.define([
     return UIComponent.extend("ui5.walkthrough.Component",{
         metadata : {
             "interfaces" : ["sap.ui.core.IAsyncContentCreation"],
-            "rootView": {
-                "viewName":"ui5.walkthrough.view.App",
-                "type" : "XML",
-                "id" : "app"
-            }
+            manifest : "json"
         },
 
         init(){
@@ -27,11 +23,6 @@ sap.ui.define([
          const oModel = new JSONModel(oData);
          this.setModel(oModel);
 
-         // set i18n model
-         const i18nModel = new ResourceModel({
-            bundleName: "ui5.walkthrough.i18n.i18n"
-         });
-         this.setModel(i18nModel, "i18n");
         }
     })
     }
