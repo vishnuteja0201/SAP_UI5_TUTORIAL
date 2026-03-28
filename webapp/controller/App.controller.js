1508bc258d563cbddef4127d914b865aa7e8e782
vishnuteja0201 <vishnuteja0201@gmail.com>
2026-03-28 17:38:23 +0530

    step15_nestedviews

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,31 +1,9 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel"
-], (Controller, MessageToast, JSONModel, ResourceModel) => {
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
     "use strict";
 
     return Controller.extend("ui5.walkthrough.controller.App",{
-        
-        onShowHello(){
-            //"Read msg from i18n model"
-            const oBundle = this.getView().getModel("i18n").getResourceBundle();
-            const sRecipient = this.getView().getModel().getProperty("/recipient/name");
-            const sMsg = oBundle.getText("helloMsg",[sRecipient]);
-            //show message
-            MessageToast.show(sMsg);
-        },
-
-        onBye(){
-            //show a native JavaScipt alert
-            alert("seee you soon");
-            console.log("End of controller.js.........!!!");
-
-        }
-
 
     });
-}
-
-)
+});
---
+++ webapp/controller/HelloPanel.controller.js
@@ -0,0 +1,29 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+], (Controller, MessageToast) => {
+    "use strict";
+
+    return Controller.extend("ui5.walkthrough.controller.HelloPanel",{
+        
+        onShowHello(){
+            //"Read msg from i18n model"
+            const oBundle = this.getView().getModel("i18n").getResourceBundle();
+            const sRecipient = this.getView().getModel().getProperty("/recipient/name");
+            const sMsg = oBundle.getText("helloMsg",[sRecipient]);
+            //show message
+            MessageToast.show(sMsg);
+        },
+
+        onBye(){
+            //show a native JavaScipt alert
+            alert("seee you soon");
+            console.log("End of controller.js.........!!!");
+
+        }
+
+
+    });
+}
+
+)
