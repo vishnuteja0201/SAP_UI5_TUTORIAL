29bc0afe5c2d4b742c14a0ae89482cb79f8af655
vishnuteja0201 <vishnuteja0201@gmail.com>
2026-04-01 02:04:15 +0530

    step23_filtering

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -1,7 +1,9 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/ui/model/json/JSONModel"
-], (Controller, JSONModel) => {
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator"
+], (Controller, JSONModel, Filter, FilterOperator) => {
 	"use strict";
 
 	return Controller.extend("ui5.walkthrough.controller.InvoiceList", {
@@ -10,6 +12,22 @@ sap.ui.define([
 				currency: "EUR"
 			});
 			this.getView().setModel(oViewModel, "view");
+		},
+
+		 onFilterInvoices(oEvent){
+			//build filter array
+			const afilter = [];
+			const sQuery = oEvent.getParameter("query");
+			if(sQuery){
+				afilter.push(new Filter("ProductName", FilterOperator.Contains, sQuery));
+			}
+			console.log(afilter);
+
+			//filter binding
+			const oList = this.byId("InvoiceList");
+			const oBinding = oList.getBinding("items");
+			oBinding.filter(afilter);
+			//console.log(oEvent);
 		}
 	});
 });
